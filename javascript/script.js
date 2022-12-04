b73584b9d7244eacc68859a42014a634575d6548
smarceta <smarceta202@gmail.com>
2022-12-03 20:26:44 -0500

    Added event listeners to update mobile CC

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,12 @@
 
-// Credit Card text
+// Credit Card text elements
 cardFrontName = document.getElementById('full_name');
 cardFrontNumber = document.getElementById('cc_number')
 cardFrontMonth = document.getElementById('card_front_month');
 cardFrontYear = document.getElementById('card_front_year');
 cardBackCvc = document.getElementById('card-back-cvc');
 
-// Form inputs
+// Form input elements
 fullName = document.getElementById('cardholder_name');
 cardNumb = document.getElementById('card_number');
 expMonth = document.getElementById('month');
@@ -14,26 +14,99 @@ expYear = document.getElementById('year');
 cardCvc = document.getElementById('cvc');
 
 
-// Event listeners for input
-fullName.addEventListener('keydown ', (event) => {
-    cardFrontName.innerHTML += event.key;
-    console.log(event);
+legalName = ""
+cc = ""
+m = ""
+y = ""
+cvc = ""
+
+// Event listeners for Legal Name
+fullName.addEventListener('keypress', (event) => {
+    legalName += event.key;
+    cardFrontName.innerHTML = legalName;
 })
 
-cardNumb.addEventListener('keydown ', (event) => {
-    cardFrontNumber.innerHTML += event.key;
+fullName.addEventListener('keydown', (event) => {
+
+    if(event.key === "Backspace" || event.key === "Delete"){
+
+            legalName = legalName.slice(0, -1);
+            cardFrontName.innerHTML = legalName;
+    }
 })
 
-expMonth.addEventListener('keydown ', (event) => {
-    cardFrontMonth.innerHTML += event.key;
 
-    
+// Event listener for cc number
+cardNumb.addEventListener('keypress', (event) => {
+
+    if(cc.length < 19 ){
+    cc += event.key;
+    cardFrontNumber.innerHTML = cc;
+    }
 })
 
-expYear.addEventListener('keydown ', (event) => {
-    cardFrontYear.innerHTML += event.key;
+cardNumb.addEventListener('keydown', (event) => {
+
+    if(event.key === "Backspace" || event.key === "Delete"){
+
+            cc = cc.slice(0, -1);
+            cardFrontNumber.innerHTML = cc;
+    }
 })
 
-cardCvc.addEventListener('keydown ', (event) => {
-    cardBackCvc.innerHTML += event.key;
+
+
+// Event listener for month 
+expMonth.addEventListener('keypress', (event) => {
+
+    if(m.length < 2 ){
+        m += event.key; 
+        cardFrontMonth.innerHTML = m ;
+    }
+})
+
+expMonth.addEventListener('keydown', (event) => {
+
+    if(event.key === "Backspace" || event.key === "Delete"){
+
+            m = m.slice(0, -1);
+            cardFrontMonth.innerHTML = m;
+    }
+})
+
+
+// Event listener for year 
+expYear.addEventListener('keypress', (event) => {
+
+    if(y.length < 2 ){
+        y += event.key; 
+        cardFrontYear.innerHTML = y ;
+    }
+})
+
+expYear.addEventListener('keydown', (event) => {
+
+    if(event.key === "Backspace" || event.key === "Delete"){
+
+            y = y.slice(0, -1);
+            cardFrontYear.innerHTML = y;
+    }
+})
+
+// Event listener for cvc 
+cardCvc.addEventListener('keypress', (event) => {
+
+    if(cvc.length < 3 ){
+        cvc += event.key; 
+        cardBackCvc.innerHTML = cvc ;
+    }
+})
+
+cardCvc.addEventListener('keydown', (event) => {
+
+    if(event.key === "Backspace" || event.key === "Delete"){
+
+        cvc = cvc.slice(0, -1);
+            cardBackCvc.innerHTML = cvc;
+    }
 })
